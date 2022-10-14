5372f5f0fd7da688d17a2decd57ee8a2c3798336
Nikolay Hizanishvili <nikolay.hizanishvili@gmail.com>
2022-10-14 21:57:28 +0300

    Add read and insert orders for Database

--- service/database/database.go
+++ service/database/database.go
@@ -2,14 +2,23 @@ package database
 
 import (
 	"context"
+	"wbintern/l0/service/model"
 
 	"github.com/jackc/pgx/v5"
 )
 
+// Database connection
 type Database struct {
 	Connection *pgx.Conn
 }
 
+// Insert an order into a database
+func (db *Database) InsertOrder(om model.OrderModel) error {
+	_, err := db.Connection.Exec(context.Background(), `insert into orders (id, data) values ($1, $2)`, om.Uid, om.Json)
+	return err
+}
+
+// Establish database connection
 func Connect(connStr string) (*Database, error) {
 	var database Database
 	dbconn, err := pgx.Connect(context.Background(), connStr)
@@ -18,4 +27,23 @@ func Connect(connStr string) (*Database, error) {
 	}
 	database.Connection = dbconn
 	return &database, err
+}
+
+// Return all orders from a database as a slice
+func (db *Database) GetOrders() ([]model.OrderModel, error) {
+	raw, err := db.Connection.Query(context.Background(), `select * from orders`)
+	if err != nil {
+		return []model.OrderModel{}, err
+	}
+
+	rows := []model.OrderModel{}
+	for raw.Next() {
+		row := model.OrderModel{}
+		err := raw.Scan(&row.Uid, &row.Json)
+		if err != nil {
+			return rows, err
+		}
+		rows = append(rows, row)
+	}
+	return rows, err
 }
